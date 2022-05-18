b59525512472b97cf8f1e5a6d3ab053e1001ebac
suchang <droplets20@gmail.com>
2022-05-18 16:21:01 +0900

    update

---
+++ demo/src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employees")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String location;
+
+    private String department;
+}
