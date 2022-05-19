0ffb9a62f4684a860f1d7818d1944ba7217afe06
suchang <droplets20@gmail.com>
2022-05-19 16:46:23 +0900

    update axios

--- demo-react/src/http-common.js
+++ demo-react/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://13.124.210.86:8080/api/v1',
+    baseURL: 'http://localhost:8080/api/v1',
     headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin':'*',
